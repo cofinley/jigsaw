275f1de908feb0b354cbf68304f97084b41bfd48
Connor Finley <connor.finley5@gmail.com>
2025-01-05 13:49:57 -0500

    Account for not output-piano label type keys in incoming node

--- src/jigsaw/views.cljs
+++ src/jigsaw/views.cljs
@@ -101,30 +101,34 @@
 
 (defn output-piano-node [props _]
   (let [incoming-nodes (re-frame/subscribe [::subs/incoming props])
-        selected-label (r/atom :pitches)]
+        selected-label (r/atom :pitches)
+        label-types [:pitches :intervals :degrees]]
     (fn [props _]
       (r/as-element
        [node {:props props :title "Piano"}
         [handle {:type "target" :position "left"}]
         (if-let [incoming-node (first @incoming-nodes)]
-          (let [notes (get-in incoming-node [:data :notes])]
+          (let [data (:data incoming-node)
+                notes (:notes data)]
             (if (seq notes)
-              (let [midis (map algo/note->midi notes) midi->label (zipmap midis (get-in incoming-node [:data @selected-label]))
+              (let [midis (map algo/note->midi notes)
+                    midi->label (zipmap midis (get-in incoming-node [:data @selected-label]))
                     first-midi (first midis)
                     last-midi (last midis)
                     midi-range-start (- first-midi (mod first-midi 12))
                     midi-range-end (dec (+ last-midi (- 12 (mod last-midi 12))))
                     width (* key-width (- midi-range-end midi-range-start))]
                 [:<>
-                 [:div {:class "self-start flex space-x-2 items-center mb-2"}
-                  [:label "Key Labels"]
-                  [select {:value (or @selected-label "")
-                           :class "text-black"
-                           :on-change #(reset! selected-label (keyword (-> % .-target .-value)))
-                           :placeholder "Key Labels"}
-                   (for [label-type [:pitches :intervals :degrees]
-                         :when (contains? (:data incoming-node) label-type)]
-                     ^{:key label-type} [:option (name label-type)])]]
+                 (when (some (partial contains? data) label-types)
+                   [:div {:class "self-start flex space-x-2 items-center mb-2"}
+                    [:label "Key Labels"]
+                    [select {:value (or @selected-label "")
+                             :class "text-black"
+                             :on-change #(reset! selected-label (keyword (-> % .-target .-value)))
+                             :placeholder "Key Labels"}
+                     (for [label-type label-types
+                           :when (contains? (:data incoming-node) label-type)]
+                       ^{:key label-type} [:option (name label-type)])]])
                  [:div {:style {:pointerEvents "none"}}
                   [:> Piano
                    {:noteRange {:first midi-range-start :last midi-range-end}
