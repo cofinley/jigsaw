bffba5e407f345a078b2c5c35c14b27a47427f3c
Connor Finley <connor.finley5@gmail.com>
2025-06-27 22:23:02 -0400

    Auto-scroll to table row on mount if shape already loaded/selected

--- src/jigsaw/components/table.cljs
+++ src/jigsaw/components/table.cljs
@@ -1,7 +1,9 @@
 (ns jigsaw.components.table
   (:require
    [clojure.string :as string]
-   [jigsaw.spec :as specs]))
+   [jigsaw.spec :as specs]
+   [reagent.core :as r]
+   ["react" :refer [useEffect useRef]]))
 
 (defn is-draggable-row? [m]
   (specs/shape-ref? m))
@@ -20,43 +22,70 @@
   (-> e .-target .-classList (.remove "opacity-50"))
   (-> e .-target .-classList (.remove "cursor-grabbing")))
 
-(defn table [{:keys [ms
-                     row-render
-                     row-selected?
-                     on-row-hover
-                     on-row-click
-                     row-title-render
-                     key-fn
-                     row-filter]}]
-  [:div {:class "max-h-96 w-full overflow-scroll nowheel nodrag flex flex-col"}
-   [:table
-    [:thead
-     [:tr {:class "sticky w-full top-0 bg-neutral-700 z-10"}
-      (doall (for [header (keys row-render)]
-               ^{:key (str "header-" header)}
-               [:th {:class "text-xl"} header]))]]
-    [:tbody
-     (doall
-      (for [m ms
-            :when (if (some? row-filter)
-                    (row-filter m)
-                    true)]
-        (let [selected? (and (some? row-selected?) (row-selected? m))
-              draggable? (is-draggable-row? m)]
-          ^{:key (or (and (some? key-fn) (key-fn m)) m)}
-          [:tr (cond-> {:class (string/join
-                                " "
-                                [(if selected?
-                                   "bg-indigo-500 hover:bg-indigo-400"
-                                   "bg-neutral-800 even:bg-neutral-900 hover:bg-neutral-700")
-                                 "hover:cursor-pointer"
-                                 (when draggable? "cursor-grab")])
-                        :onMouseOver #(when on-row-hover (on-row-hover m))
-                        :onClick #(when on-row-click (on-row-click m))
-                        :title (when row-title-render (row-title-render m))}
-                 draggable? (assoc :draggable true
-                                   :onDragStart #(handle-drag-start m %)
-                                   :onDragEnd handle-drag-end))
-           (doall (for [[header col-render-fn] row-render]
-                    ^{:key (str "row-" header)}
-                    [:td {:class "text-xl"} (col-render-fn m)]))])))]]])
+(defn table-impl [{:keys [ms
+                          row-render
+                          row-selected?
+                          on-row-hover
+                          on-row-click
+                          row-title-render
+                          key-fn
+                          row-filter]}]
+  (let [container-ref (useRef nil)
+        selected-row-ref (useRef nil)]
+
+    ;; Auto-scroll to selected row on mount
+    (useEffect
+     (fn []
+       (when (and (.-current container-ref) (.-current selected-row-ref))
+         (let [container (.-current container-ref)
+               selected-row (.-current selected-row-ref)
+               container-height (.-clientHeight container)
+               container-scroll-top (.-scrollTop container)
+               row-offset-top (.-offsetTop selected-row)
+               row-height (.-clientHeight selected-row)]
+           ;; Scroll if the selected row is not fully visible
+           (when (or (< row-offset-top container-scroll-top)
+                     (> (+ row-offset-top row-height) (+ container-scroll-top container-height)))
+             (set! (.-scrollTop container)
+                   (- row-offset-top (/ (- container-height row-height) 2))))))
+       ;; Cleanup function (not needed in this case)
+       (fn []))
+     #js [])
+
+    [:div {:ref container-ref
+           :class "max-h-96 w-full overflow-scroll nowheel nodrag flex flex-col"}
+     [:table
+      [:thead
+       [:tr {:class "sticky w-full top-0 bg-neutral-700 z-10"}
+        (doall (for [header (keys row-render)]
+                 ^{:key (str "header-" header)}
+                 [:th {:class "text-xl"} header]))]]
+      [:tbody
+       (doall
+        (for [m ms
+              :when (if (some? row-filter)
+                      (row-filter m)
+                      true)]
+          (let [selected? (and (some? row-selected?) (row-selected? m))
+                draggable? (is-draggable-row? m)]
+            ^{:key (or (and (some? key-fn) (key-fn m)) m)}
+            [:tr (cond-> {:ref (when selected? selected-row-ref)
+                          :class (string/join
+                                  " "
+                                  [(if selected?
+                                     "bg-indigo-500 hover:bg-indigo-400"
+                                     "bg-neutral-800 even:bg-neutral-900 hover:bg-neutral-700")
+                                   "hover:cursor-pointer"
+                                   (when draggable? "cursor-grab")])
+                          :onMouseOver #(when on-row-hover (on-row-hover m))
+                          :onClick #(when on-row-click (on-row-click m))
+                          :title (when row-title-render (row-title-render m))}
+                   draggable? (assoc :draggable true
+                                     :onDragStart #(handle-drag-start m %)
+                                     :onDragEnd handle-drag-end))
+             (doall (for [[header col-render-fn] row-render]
+                      ^{:key (str "row-" header)}
+                      [:td {:class "text-xl"} (col-render-fn m)]))])))]]]))
+
+(defn table [props]
+  [:f> table-impl props])
